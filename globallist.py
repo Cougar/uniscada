b6d306294731e539c6a3587cd34045dcd0e72e92
Cougar <cougar@random.ee>
2014-09-30 21:15:41 +0300

    Add GlobalList class
    GlobalList class is for grouping other objects based
    on some `id`. It implements basic functions to add, remove,
    get and list these objects

---
+++ globallist.py
@@ -0,0 +1,59 @@
+''' Keep track of list of objects identified by some id
+'''
+import threading
+
+import logging
+log = logging.getLogger(__name__)
+
+__all__ = [
+    'GlobalList', 'find_by_id', 'remove_by_id', 'get_id_list',
+]
+
+class GlobalList(object):
+    ''' List of all members '''
+
+    def __init__(self):
+        self._members = {}
+        self.class_name = self.getMemberClass()
+        log.debug('Initialise a new global list of ' + str(self.class_name))
+
+    def getMemberClass(self):
+        ''' Return a class name that should be used for members
+        '''
+        raise Exception("please implement getMemberClass() method")
+
+    def find_by_id(self, id):
+        ''' Return a list member or if there is no existing member
+        with that id, then a new member instance will be created.
+
+        :param id: id
+
+        :returns: member instance
+        '''
+        log.debug('find_by_id(%s)', str(id))
+        if not id in self._members:
+            log.info('  create a new member(%s)', str(id))
+            self._members[id] = self.class_name(id)
+        log.error('NEW INSTANCE: ' + str(self._members[id]))
+        return self._members[id]
+
+    def get_id_list(self):
+        ''' Generates a list of member instances
+
+        :returns: Generated instance for each member
+        '''
+        for id in self._members.keys():
+            yield(id)
+
+    def remove_by_id(self, id):
+        log.info('remove_by_id(%s)', str(id))
+        if id in self._members:
+            del self._members[id]
+        else:
+            log.error('  no such client')
+
+    def __str__(self):
+        s = ''
+        for id in self.get_id_list():
+            s += '\n  ' + str(self.find_by_id(id))
+        return(s)
---
+++ test/test_globallist.py
@@ -0,0 +1,55 @@
+import unittest
+
+from globallist import GlobalList
+
+class MemberClass(object):
+    def __init__(self, id):
+        self.id = id
+
+class DerivedGlobalList(GlobalList):
+    def getMemberClass(self):
+        return MemberClass
+
+class GlobalListTests(unittest.TestCase):
+    '''
+    This is the unittest for the uniscada.globallist module
+    '''
+    def setUp(self):
+        self.globallist = DerivedGlobalList()
+
+    def test_class_name(self):
+        ''' Test member class '''
+        self.assertEqual(self.globallist.getMemberClass(), MemberClass)
+
+    def test_member_id(self):
+        ''' Test member instance setup '''
+        id1 = self.globallist.find_by_id('A')
+        self.assertEqual(id1.id, 'A')
+
+    def test_add_member(self):
+        ''' Test member add and lookup '''
+        id1 = self.globallist.find_by_id('A')
+        id2 = self.globallist.find_by_id('B')
+        id3 = self.globallist.find_by_id('A')
+        self.assertTrue(isinstance(id1, MemberClass))
+        self.assertNotEqual(id1, id2)
+        self.assertEqual(id1, id3)
+        self.assertTrue(isinstance(id3, MemberClass))
+
+    def test_remove_member(self):
+        ''' Test member remove '''
+        id1 = self.globallist.find_by_id('A')
+        self.globallist.remove_by_id('A')
+        id2 = self.globallist.find_by_id('B')
+        id3 = self.globallist.find_by_id('A')
+        self.assertTrue(isinstance(id1, MemberClass))
+        self.assertNotEqual(id1, id2)
+        self.assertNotEqual(id1, id3)
+        self.assertTrue(isinstance(id3, MemberClass))
+
+    def test_member_list(self):
+        ''' Test member list generator '''
+        id1 = self.globallist.find_by_id('A')
+        id2 = self.globallist.find_by_id('B')
+        id3 = self.globallist.find_by_id('A')
+        self.assertItemsEqual(['A', 'B'], list(self.globallist.get_id_list()))
