e22f855261f16675cb1a1d5470643b5d60b43497
Cougar <cougar@random.ee>
2014-10-05 23:43:09 +0300

    Use NagiosUser in UserSession to fetch userdata

--- usersession.py
+++ usersession.py
@@ -1,6 +1,8 @@
 ''' User data structure
 '''
 
+from nagiosuser import NagiosUser
+
 import logging
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -8,6 +10,7 @@ log.addHandler(logging.NullHandler())
 __all__ = [
     'UserSession',
     'get_id',
+    'get_userdata',
 ]
 
 class UserSession(object):
@@ -20,6 +23,8 @@ class UserSession(object):
         '''
         log.debug('Create a new user (%s)', str(id))
         self._id = id
+        self._userdata = None
+        self._userdata_callback = None
 
     def get_id(self):
         ''' Get id of user (username)
@@ -28,5 +33,42 @@ class UserSession(object):
         '''
         return self._id
 
+    def read_userdata_form_nagois(self, callback = None):
+        ''' Read userdata from Nagios
+
+            Call callback() when response from Nagios is received
+
+            :param callback: optional callback function
+        '''
+        self._userdata_callback = callback
+        NagiosUser(self._id).async_load_userdata(self._userdata_from_nagios)
+
+    def _userdata_from_nagios(self, userdata):
+        ''' Process Nagios reply with userdata
+
+        After setting userdata, call _userdata_callback() if set
+
+        :param userdata: userdata from Nagios
+        '''
+        self._set_userdata(userdata)
+        if self._userdata_callback:
+            self._userdata_callback()
+            self._userdata_callback = None
+
+    def _set_userdata(self, userdata):
+        ''' Set userdata. This method can be used as a callback
+
+        :param userdata: userdata
+        '''
+        self._userdata = userdata
+
+    def get_userdata(self):
+        ''' Return userdata or None if not initialised
+
+        :returns: userdata or None
+        '''
+        return self._userdata
+
     def __str__(self):
-        return(str(self._id))
+        return(str(self._id) + ': ' +
+            'userdata = ' + str(self._userdata))
