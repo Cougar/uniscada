ecaef01facb02503a30e1a064e3a3182eed39e1d
Cougar <cougar@random.ee>
2014-10-12 14:43:05 +0300

    Raise exception if unknown data is in List of Values

--- sdp.py
+++ sdp.py
@@ -50,7 +50,10 @@ class SDP(object):
         elif key[-1] == 'W':
             if not isinstance(val, str):
                 raise Exception('List of Values _MUST_BE_ string of numbers')
-            if val != ' '.join(list(map(str, (list(map(int, val.split(' '))))))):
+            try:
+                if val != ' '.join(list(map(str, (list(map(int, val.split(' '))))))):
+                    raise Exception('Only integers allowed in List of Values')
+            except:
                 raise Exception('Only integers allowed in List of Values')
             self.add_value(key[:-1], list(map(int, val.split(' '))))
         else:
