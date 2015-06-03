df9788f57405db4769517003a5585d0bcb1d77a5
Cougar <cougar@random.ee>
2015-06-03 10:27:27 +0300

    Refactor based on pylint suggestions

--- sql2json.py
+++ sql2json.py
@@ -1,16 +1,16 @@
-''' JSON dump of SQLite database
+""" JSON dump of SQLite database
 
 Input can be SQLite database file or SQLite dump text file
 
 Output can be JSON string or JSON file(s)
-'''
+"""
 
 import sqlite3
 import sys
 import json
 
 import logging
-log = logging.getLogger(__name__)
+log = logging.getLogger(__name__)   # pylint: disable=invalid-name
 log.addHandler(logging.NullHandler())
 
 
@@ -20,30 +20,31 @@ class SQLdb(object):
         self.conn = None
 
     def setconn(self, conn):
-        ''' Set SQLite connection
-        
+        """ Set SQLite connection
+
         :param conn: SQLite connection
-        '''
+        """
         self.conn = conn
 
     def loaddb(self, dbfilename):
-        ''' Connect to SQLite database
+        """ Connect to SQLite database
 
         :param dbfilename: SQLite database file name
-        '''
+        """
         try:
             conn = sqlite3.connect(dbfilename)
-        except Exception as e:
-            log.critical('cant connect to SQLite db %s: %s', dbfilename, str(sys.exc_info()[1]))
-            raise Exception(e)
+        except Exception as ex:
+            log.critical('cant connect to SQLite db %s: %s', \
+                dbfilename, str(sys.exc_info()[1]))
+            raise Exception(ex)
         self.setconn(conn)
 
     def gettablelines(self, table):
-        ''' Get list of table line dictionaries
+        """ Get list of table line dictionaries
 
         :param table: table name
         :returns list of table line dictionaries
-        '''
+        """
         if not self.conn:
             log.critical('db is not connected')
             raise Exception()
@@ -52,38 +53,41 @@ class SQLdb(object):
             cur = self.conn.cursor()
             cur.execute('select * from %s' % table)
             for row in cur:
-                yield(dict(row))
-        except Exception as e:
-            log.critical('cant read sql table %s: %s', table, str(sys.exc_info()[1]))
-            raise Exception(e)
+                yield dict(row)
+        except Exception as ex:
+            log.critical('cant read sql table %s: %s', \
+                table, str(sys.exc_info()[1]))
+            raise Exception(ex)
 
 class SQLdump(object):
     def __init__(self, dumpfile):
-        ''' Read SQLite dump and create in-memory database
+        """ Read SQLite dump and create in-memory database
 
         :param dumpfile: SQLite dump file name
-        '''
-        log.info('Load SQLite from dump file %s' % dumpfile)
+        """
+        log.info('Load SQLite from dump file %s', dumpfile)
         self.conn = None
         try:
             sql = open(dumpfile, encoding='utf-8').read()
-        except Exception as e:
-            log.critical('cant load SQLite dump from %s: %s', dumpfile, str(sys.exc_info()[1]))
-            raise Exception(e)
+        except Exception as ex:
+            log.critical('cant load SQLite dump from %s: %s', \
+                dumpfile, str(sys.exc_info()[1]))
+            raise Exception(ex)
         try:
             conn = sqlite3.connect(':memory:')
             conn.executescript(sql)
             conn.commit()
-        except Exception as e:
-            log.critical('cant import SQLite dump %s: %s', dumpfile, str(sys.exc_info()[1]))
-            raise Exception(e)
+        except Exception as ex:
+            log.critical('cant import SQLite dump %s: %s', \
+                dumpfile, str(sys.exc_info()[1]))
+            raise Exception(ex)
         self.conn = conn
 
     def getconn(self):
-        ''' Get SQLite connection
+        """ Get SQLite connection
 
         :returns SQLite connection instance
-        '''
+        """
         return self.conn
 
 class JSONdumper(object):
@@ -91,7 +95,7 @@ class JSONdumper(object):
         pass
 
     def dump(self, rows, field, value, dir, skipempty, skipfields, jsonmin, array, output):
-        ''' Dump database rows in JSON format
+        """ Dump database rows in JSON format
 
         :param rows: generator or database rows
         :param field: Field name for filename
@@ -102,13 +106,13 @@ class JSONdumper(object):
         :param jsonmin: Minimal JSON format
         :param array: Display as JSON array
         :param output: Write ouptut to file
-        '''
+        """
         a = []
         for row in rows:
             if skipfields:
                 for f in skipfields.split(','):
                     if not f in row:
-                        log.warning('no such field in row: %s' % f)
+                        log.warning('no such field in row: %s', f)
                     else:
                         del row[f]
 
@@ -119,11 +123,11 @@ class JSONdumper(object):
 
             if value or dir:
                 if not field in row:
-                    log.critical('no such field in row: %s' % field)
-                    raise Exception(e)
+                    log.critical('no such field in row: %s', field)
+                    raise Exception
 
             if value and row[field] != value:
-                    continue
+                continue
 
             if array:
                 a.append(row)
@@ -131,11 +135,11 @@ class JSONdumper(object):
                 if jsonmin:
                     s = json.dumps(row)
                 else:
-                    s= json.dumps(row, indent=4, sort_keys=True)
+                    s = json.dumps(row, indent=4, sort_keys=True)
 
                 if dir:
                     print("WRITE: %s/%s" % (dir, row[field]))
-                    with open("%s/%s" % (dir, row[field]) , "w") as f:
+                    with open("%s/%s" % (dir, row[field]), "w") as f:
                         f.write(s)
                 else:
                     if output:
@@ -148,7 +152,7 @@ class JSONdumper(object):
             if jsonmin:
                 s = json.dumps(a)
             else:
-                s= json.dumps(a, indent=4, sort_keys=True)
+                s = json.dumps(a, indent=4, sort_keys=True)
             if output:
                 with open(output, "w") as f:
                     f.write(s)
@@ -159,17 +163,17 @@ class JSONdumper(object):
 if __name__ == '__main__':
     from tornado.options import define, options, parse_command_line
 
-    define("db", default = "", help = "Database filename", type = str)
-    define("dbdump", default = "", help = "Database dump filename", type = str)
-    define("table", default = "", help = "Table name", type = str)
-    define("field", default = "", help = "Field name for filename", type = str)
-    define("value", default = "", help = "Dump only one row where field value matches", type = str)
-    define("dir", default = "", help = "JSON dump direcory name", type = str)
-    define("jsonmin", default = False, help = "Minimal JSON format", type = bool)
-    define("skipempty", default = False, help = "Do not dump empty fields", type = bool)
-    define("skipfields", default = "", help = "Comma separated field names to skip", type = str)
-    define("array", default = False, help = "Display as JSON array", type = bool)
-    define("output", default = "", help = "Write ouptut to file", type = str)
+    define("db", default="", help="Database filename", type=str)
+    define("dbdump", default="", help="Database dump filename", type=str)
+    define("table", default="", help="Table name", type=str)
+    define("field", default="", help="Field name for filename", type=str)
+    define("value", default="", help="Dump only one row where field value matches", type=str)
+    define("dir", default="", help="JSON dump direcory name", type=str)
+    define("jsonmin", default=False, help="Minimal JSON format", type=bool)
+    define("skipempty", default=False, help="Do not dump empty fields", type=bool)
+    define("skipfields", default="", help="Comma separated field names to skip", type=str)
+    define("array", default=False, help="Display as JSON array", type=bool)
+    define("output", default="", help="Write ouptut to file", type=str)
 
     args = sys.argv
     args.append("--logging=error")
