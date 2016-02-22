ac2afe8e322946b92f7219a1735baf5fa28361d4
Cougar <cougar@random.ee>
2016-02-22 23:21:23 +0200

    Move API URL to variable

--- create_controllers.sh
+++ create_controllers.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+BASEURL="http://127.0.0.1/api/v1"
+
 for controller in ${*:-$(ls conf/controller)}; do
     echo -n "$controller: create .."
-    curl --silent --request POST --data '[ { "controller": "'$controller'" } ]' http://127.0.0.1/api/v1/controllers/ && echo -n " OK" || echo -n " ERROR"
+    curl --silent --request POST --data '[ { "controller": "'$controller'" } ]' ${BASEURL}/controllers/ && echo -n " OK" || echo -n " ERROR"
     echo -n " config .."
-    curl --silent --request PUT --data @./conf/controller/$controller http://127.0.0.1/api/v1/controllers/$controller && echo -n " OK" || echo -n " ERROR"
+    curl --silent --request PUT --data @./conf/controller/$controller ${BASEURL}/controllers/$controller && echo -n " OK" || echo -n " ERROR"
     echo
 done
--- create_servicegroups.sh
+++ create_servicegroups.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+BASEURL="http://127.0.0.1/api/v1"
+
 for servicegroup in ${*:-$(ls conf/servicegroup)}; do
     echo -n "$servicegroup: create .."
-    curl --silent --request POST --data '[ { "servicegroup": "'$servicegroup'" } ]' http://127.0.0.1/api/v1/servicegroups/ && echo -n " OK" || echo -n " ERROR"
+    curl --silent --request POST --data '[ { "servicegroup": "'$servicegroup'" } ]' ${BASEURL}/servicegroups/ && echo -n " OK" || echo -n " ERROR"
     echo -n " config .."
-    curl --silent --request PUT --data @./conf/servicegroup/$servicegroup http://127.0.0.1/api/v1/servicegroups/$servicegroup && echo -n " OK" || echo -n " ERROR"
+    curl --silent --request PUT --data @./conf/servicegroup/$servicegroup ${BASEURL}/servicegroups/$servicegroup && echo -n " OK" || echo -n " ERROR"
     echo
 done
 
