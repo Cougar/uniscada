5ba1ad810b112a47ad2b6b580c4281bc4fbe72cc
Cougar <cougar@random.ee>
2016-03-21 00:31:44 +0200

    Add APIBase._error_if_not_scope()

--- apibase.py
+++ apibase.py
@@ -52,10 +52,14 @@ class APIBase(object):
 
     def _error_if_not_systemuser(self, **kwargs):
         """ Check if request came from 'system' user """
+        self._error_if_not_scope('system', **kwargs)
+
+    def _error_if_not_scope(self, scope, **kwargs):
+        """ Check if user is in this scope """
         usersession = self._usersessions.get_id(kwargs.get('user', None))
         assert usersession
-        if not usersession.check_scope('system'):
-            raise UserWarning('no system user permissions')
+        if not usersession.check_scope(scope):
+            raise UserWarning('no user permission: %s', scope)
 
     def _get_data_or_error(self, **kwargs):
         """ Return required data or raise Error """
