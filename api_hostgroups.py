971f3716b9adc759bb0c4e0bf324a2655e985298
Cougar <cougar@random.ee>
2016-04-02 20:06:21 +0300

    Return /api/v1/hostgroups/<hg> data in list
    Support for CSV filter

--- api_hostgroups.py
+++ api_hostgroups.py
@@ -26,29 +26,31 @@ class APIhostgroups(APIBase):
         """ Return details of one hostgroup """
         log.debug('_request_get_with_filter(%s)', str(kwargs))
         user = kwargs.get('user', None)
-        hostgroup = kwargs.get('filter', None)
         usersession = self._usersessions.find_by_id(user)
         userdata = usersession.get_userdata()
-        if not 'hostgroups' in userdata:
-            return {'status': 404}
-        if not hostgroup in userdata['hostgroups']:
-            raise SessionException('no such hostgroup for this user')
-        r = {}
-        r['hostgroup'] = hostgroup
-        r['hosts'] = []
-        if 'members' in userdata['hostgroups'][hostgroup]:
-            for host, alias in \
-                    userdata['hostgroups'][hostgroup]['members'].items():
-                if usersession.check_access(host):
-                    r1 = {'id': host, 'alias': alias}
-                    h = self._controllers.get_id(host)
-                    if h:
-                        servicegroup = h.get_setup().get('servicetable', '')
-                        r1['servicegroup'] = servicegroup
-                    else:
-                        log.warning('host "%s" with alias "%s" ' \
-                            'doesnt exists but is in user "%s" ' \
-                            'hostgroup "%s"', \
-                            host, alias, user, hostgroup)
-                    r['hosts'].append(r1)
+        r = []
+        for hostgroup in kwargs.get('filter', '').split(','):
+            if not 'hostgroups' in userdata:
+                return {'status': 404}
+            if not hostgroup in userdata['hostgroups']:
+                raise SessionException('no such hostgroup for this user')
+            hg = {}
+            hg['hostgroup'] = hostgroup
+            hg['hosts'] = []
+            if 'members' in userdata['hostgroups'][hostgroup]:
+                for host, alias in \
+                        userdata['hostgroups'][hostgroup]['members'].items():
+                    if usersession.check_access(host):
+                        r1 = {'id': host, 'alias': alias}
+                        h = self._controllers.get_id(host)
+                        if h:
+                            servicegroup = h.get_setup().get('servicetable', '')
+                            r1['servicegroup'] = servicegroup
+                        else:
+                            log.warning('host "%s" with alias "%s" ' \
+                                'doesnt exists but is in user "%s" ' \
+                                'hostgroup "%s"', \
+                                host, alias, user, hostgroup)
+                        hg['hosts'].append(r1)
+            r.append(hg)
         return {'status': 200, 'bodydata': r}
