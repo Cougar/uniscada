562ce4a6e3e743d76cf25b3b04b8613b68ac239b
Cougar <cougar@random.ee>
2016-04-10 19:01:51 +0300

    Catch SDPException and Exception separately
    This avoids situations where real problems with code remains hidden

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@
 import time
 
 from sdp import SDP
+from sdpexception import SDPException
 from stats import Stats
 
 import logging
@@ -301,12 +302,15 @@ class Controller(object):
         log.debug('set_last_sdp(%s)', str(self._id))
         try:
             self._process_incoming_sdp(sdp, ts=ts)
-        except Exception as ex:
+        except SDPException as ex:
             log.error('contoller=%s SDP processing error: %s', \
                 str(self._id),  str(ex))
             self._stats.set('rx/sdp/last_error/reason', str(ex))
             self._stats.add('rx/sdp/errors/total', 1)
-            raise Exception(ex)
+            raise SDPException(ex)
+        except Exception as ex:
+            log.exception('contoller=%s SDP processing error: %s', \
+                str(self._id),  str(ex))
         self._last_sdp = sdp
         self._last_sdp_ts = ts
         self._stats.add('rx/sdp/ok', 1)
